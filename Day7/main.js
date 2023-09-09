23e4dcc3204ef2c2771576c7741185b21d64874e
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2023-09-09 15:41:26 +0200

    DOM

--- Day7/main.js
+++ Day7/main.js
@@ -1,14 +1,32 @@
-const getUsers = () => {
-  const url = "https://randomuser.me/api/?results=10";
-  fetch(url)
+const getUsers = (e) => {
+  e.preventDefault();
+  const usersNumber = document.querySelector(`[name = "users-number"]`).value;
+  const usersGender = document.querySelector(`[name = "gender"]`).value;
+  console.log(usersNumber, usersGender);
+  const url = `https://randomuser.me/api/?results=${usersNumber}&gender=${
+    usersGender === "both" ? "male, female" : usersGender
+  }`;
+  fetch(url) // obietnica - oczekujacy (pending)
+    // obietnica musi zostac rostrzygnieta (spenione | odrzucone)
+    //wykonuje sie then - pozytywnie
     .then((response) => {
-      console.log(response);
       if (response.status !== 200) {
         throw Error("To nie jest odpowiedz 200");
       } else {
-        return response.json();
+        return response.json(); //Fetch API = json() z body wyodrebnia json
       }
     })
-    .then.catch((err) => console.log(err));
+    .then((json) => showUsers(json.results))
+    //wykonuje gdy rosztygniecie - odrzucona
+    .catch((err) => console.log(err));
 };
-document.querySelector("button").addEventListener("click", getUsers);
+const showUsers = (users) => {
+  const resultArea = document.querySelector(".user-list");
+  users.forEach((user) => {
+    const item = document.createElement("div");
+    item.className = "user";
+    item.innerHTML = `<div class="user__name"><div>
+    <img class="user__image" src=>`;
+  });
+};
+document.querySelector(".generator").addEventListener("submit", getUsers);
