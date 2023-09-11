ff86ad68aa3de596fafcadf6a439c691669f8485
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2023-09-11 23:10:35 +0200

    scroller

---
+++ Day8/scroller.js
@@ -0,0 +1,55 @@
+class Scroller {
+  constructor(rootSelector) {
+    const rootElement = document.querySelector(rootSelector);
+    this.sections = document.querySelectorAll("section");
+    const sectionsArr = [...this.sections];
+
+    const currentSectionIndex = sectionsArr.findIndex(this.isScrolledIntoView);
+    this.currentSectionIndex = Math.max(currentSectionIndex, 0);
+
+    this.isThrottled = false;
+  }
+  isScrolledIntoView(el) {
+    const rect = el.getBoundingClientRect();
+    const elemTop = rect.top;
+    const elemBottom = Math.floor(rect.bottom);
+
+    const isVissible = elemTop >= 0 && elemBottom <= window.innerHeight;
+
+    return isVissible;
+  }
+
+  listenScroll = (e) => {
+    if (this.isThrottled) return;
+    this.isThrottled = true;
+    setTimeout(() => {
+      this.isThrottled = false;
+    }, 1000);
+
+    const direction = e.wheelDelta < 0 ? 1 : -1;
+
+    this.scroll(direction);
+  };
+
+  scroll = (direction) => {
+    if (direction === 1) {
+      const isLastSection =
+        this.currentSectionIndex === this.sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const firstSection = this.currentSectionIndex === 0;
+      if (firstSection) return;
+    }
+
+    this.currentSectionIndex = this.currentSectionIndex + direction;
+
+    this.scrollToCurrentSection();
+  };
+
+  scrollToCurrentSection = () => {
+    this.sections[this.currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+}
