c452d8fa8565f99bd7bedd1a275bdd12ab8e2a0a
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2023-09-13 23:38:42 +0200

    scroller

--- Day8/scroller.js
+++ Day8/scroller.js
@@ -5,10 +5,14 @@ class Scroller {
     const sectionsArr = [...this.sections];
 
     const currentSectionIndex = sectionsArr.findIndex(this.isScrolledIntoView);
+
     this.currentSectionIndex = Math.max(currentSectionIndex, 0);
 
     this.isThrottled = false;
+
+    this.drawNavigation();
   }
+
   isScrolledIntoView(el) {
     const rect = el.getBoundingClientRect();
     const elemTop = rect.top;
@@ -19,19 +23,20 @@ class Scroller {
     return isVissible;
   }
 
-  listenScroll = (e) => {
+  listenScroll(event) {
     if (this.isThrottled) return;
     this.isThrottled = true;
+
     setTimeout(() => {
       this.isThrottled = false;
     }, 1000);
 
-    const direction = e.wheelDelta < 0 ? 1 : -1;
+    const direction = event.deltaY > 0 ? 1 : -1;
 
     this.scroll(direction);
-  };
+  }
 
-  scroll = (direction) => {
+  scroll(direction) {
     if (direction === 1) {
       const isLastSection =
         this.currentSectionIndex === this.sections.length - 1;
@@ -44,12 +49,51 @@ class Scroller {
     this.currentSectionIndex = this.currentSectionIndex + direction;
 
     this.scrollToCurrentSection();
-  };
+  }
 
-  scrollToCurrentSection = () => {
+  scrollToCurrentSection() {
+    this.selectActiveNavItem();
     this.sections[this.currentSectionIndex].scrollIntoView({
       behavior: "smooth",
       block: "start",
     });
-  };
+  }
+
+  drawNavigation() {
+    this.navigationContainer = document.createElement("aside");
+    this.navigationContainer.setAttribute("class", "scroller__navigation");
+    const list = document.createElement("ul");
+
+    this.sections.forEach((section, index) => {
+      const listItem = document.createElement("li");
+
+      listItem.addEventListener("click", () => {
+        this.currentSectionIndex = index;
+
+        this.scrollToCurrentSection();
+      });
+
+      list.appendChild(listItem);
+    });
+
+    this.navigationContainer.appendChild(list);
+
+    document.body.appendChild(this.navigationContainer);
+
+    this.selectActiveNavItem();
+  }
+
+  selectActiveNavItem() {
+    if (this.navigationContainer) {
+      const navigationItems = this.navigationContainer.querySelectorAll("li");
+
+      navigationItems.forEach((item, index) => {
+        if (index === this.currentSectionIndex) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+    }
+  }
 }
